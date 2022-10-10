8312ddcc50e00957309037e6d064b39b345afc82
cmreavis97 <cmreavis97@gmail.com>
2022-10-10 16:27:57 -0400

    Creating interfaces/classes

---
+++ ORMAndDapper_Exercise1/ORMAndDapper_Exercise1/DapperDepartmentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORMAndDapper_Exercise1
+{
+    public class DapperDepartmentRepository : IDepartmentRepository
+    {
+        //Methods for accessing the database stored here
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
